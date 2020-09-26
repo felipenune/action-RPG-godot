0a47830b0394a80a9bf5e5f0db79bd0675ba7cc3
felipenune <41213964+felipenune@users.noreply.github.com>
2020-09-26 16:54:46 -0300

    grass effect

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,11 +6,31 @@ const FRICTION = 500
 
 var velocity = Vector2.ZERO
 
+enum {
+	MOVE,
+	ROLL,
+	ATTACK
+}
+
+var state = MOVE
+
 onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
-onready var state = $AnimationTree.get("parameters/playback")
+onready var animation_state = $AnimationTree.get("parameters/playback")
+
+func _ready():
+	animation_tree.active = true
 
 func _physics_process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		ROLL:
+			pass
+		ATTACK:
+			attack_state(delta)
+	
+func move_state(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -19,10 +39,21 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animation_tree.set("parameters/idle/blend_position", input_vector)
 		animation_tree.set("parameters/run/blend_position", input_vector)
-		state.travel("run")
+		animation_tree.set("parameters/attack/blend_position", input_vector)
+		animation_state.travel("run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
-		state.travel("idle")
+		animation_state.travel("idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("ui_attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animation_state.travel("attack")
+	
+func finished_attack():
+	state = MOVE
